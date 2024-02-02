84a9c2f6a836e9baac6bcea32be04305a04860e2
Rajat Gupta <rajatguptafreelancer@gmail.com>
2024-02-02 18:04:56 +0530

    added fetch profile test case

--- src/app/layout/user-profile/userprofile.service.spec.ts
+++ src/app/layout/user-profile/userprofile.service.spec.ts
@@ -2,6 +2,7 @@ import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { UserprofileService } from './userprofile.service';
 import { HttpClient } from '@angular/common/http';
+import { of } from 'rxjs';
 
 describe('UserprofileService', () => {
   let service: UserprofileService;
@@ -24,5 +25,23 @@ describe('UserprofileService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should retrieve the user profile', () => {
+    const mockProfile = [
+      {
+        id: '',
+        isDone: false,
+        title: '',
+        description: 'test description',
+      },
+    ];
+    httpClientSpy.get.and.returnValue(of(mockProfile));
+
+    service.getUserProfile().subscribe((res: any) => {
+      expect(res).toEqual(mockProfile);
+    });
+
+    expect(httpClientSpy.get.calls.count()).toBe(1, 'one call');
   });
 });
